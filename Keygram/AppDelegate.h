7d76a9bfca12413d37c2bba30edff31d751cbde3
Keylonnie Miller <keylonniem@fb.com>
2018-07-09 15:09:03 -0700

    Initial Commit

---
+++ Keygram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Keygram
+//
+//  Created by Keylonnie Miller on 7/9/18.
+//  Copyright © 2018 Keylonnie Miller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
